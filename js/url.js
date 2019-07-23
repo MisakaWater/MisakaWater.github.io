c5d6b3e82e72d09943cfd30225a538fb993b3dc5
MisakaWater <loneliestwater@gmail.com>
2019-07-23 23:10:34 +0800

    Site updated: 2019-07-23 23:10:30

--- js/url.js
+++ js/url.js
@@ -1,9 +1,9 @@
 //修改url的方法
 //sub:数组下标
 //arr:表情数组
-async function setUrl(sub, arr) {
+async function setUrl(text) {
     if (!!(window.history && history.pushState)) {
-        history.replaceState(null, null, window.location.href.split('#')[0] = arr[sub]);
+        history.replaceState(null, null, window.location.href.split('#')[0] = text);
     }
 }
 //修改url为默认url
@@ -11,6 +11,20 @@ async function cleanUrl(url) {
     if (!!(window.history && history.pushState)) {
         history.replaceState(null, null, window.location.href.split('#')[0] = url);
     }
+}
+
+function getPageHeight() {
+    let body = document.body,
+        html = document.documentElement;
+
+    let height = Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight);
+    return height;
+}
+
+function getWindowHeight() {
+    let height = window.innerHeight;
+    return height;
 }
 
 //循环动画使用的表情
@@ -29,12 +43,70 @@ var emoji = [
     "🍺"
 ]
 
-var IntervalId = null;
+var bar = [
+    "-",
+    "0"
+]
+
 var defUrl = window.location.href.split('#')[0]; //默认url
 
-//循环动画
-function UrlLoop(speed, arr, defUrl) {
+var scrollProgressIntervalId = null;
 
+function scrollBarClick() {
+    UrlProgress(bar, 0, 1, 50, -1);
+    let e = window.event;
+    if (e != undefined) {
+        obj = e.target || e.srcElement;
+    }
+    if (e.path[0].innerText == "click") {
+        e.path[0].innerText = "stop";
+        window.addEventListener('scroll', e => scrollProgress(bar), true);
+    } else {
+        alert("需要停止请刷新!");
+        e.path[0].innerText = "click";
+        window.removeEventListener('scroll', scrollProgress(bar), true);
+    }
+
+
+}
+
+
+
+function scrollProgress(arr) {
+    let now;
+
+    let scrollAvail = getPageHeight() - getWindowHeight(); // 可滚动的高度
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+
+    let length = 50;
+    now = (scrollTop / scrollAvail) * length;
+    now = parseInt(now);
+    UrlProgress(arr, 0, 1, 50, now);
+}
+//arr 对应前景，背景数组
+//subBack 进度条背景
+//subFore 进度条前景
+//length 进度条总长
+//now 当前进度
+function UrlProgress(arr, subBack, subFore, length, now) {
+    let args = "";
+    for (let i = 0; i < length; i++) {
+        args = args + arr[subBack]
+    }
+
+    if (now >= 0) {
+        args = args.split('');
+        args.splice(now, 1, arr[subFore]);
+        args = args.join('');
+    }
+
+    setUrl(args);
+}
+
+//循环动画
+var UrlLoopIntervalId = null;
+
+function UrlLoop(speed, arr, defUrl) {
     var e = window.event;
     if (e != undefined) {
         obj = e.target || e.srcElement;
@@ -43,42 +115,20 @@ function UrlLoop(speed, arr, defUrl) {
     //上面:
     //选择触发方法的元素
 
-    if (IntervalId == null) {
+    if (UrlLoopIntervalId == null) {
         this.speed = speed || '1000';
         var i = 0;
-        IntervalId = setInterval(function () {
-            setUrl(i, arr);
+        UrlLoopIntervalId = setInterval(function () {
+            setUrl(arr[i]);
             i++;
             if (i == arr.length) {
                 i = 0;
             }
         }, speed);
     } else {
-        clearInterval(IntervalId);
+        clearInterval(UrlLoopIntervalId);
         cleanUrl(defUrl);
-        IntervalId = null;
+        UrlLoopIntervalId = null;
         e.path[0].innerText = "click";
-    }
-}
-
-function print_nav_timing_data() {
-    // Use getEntriesByType() to just get the "navigation" events
-    var perfEntries = performance.getEntriesByType("navigation");
-
-    for (var i = 0; i < perfEntries.length; i++) {
-        console.log("= Navigation entry[" + i + "]");
-        var p = perfEntries[i];
-        // dom Properties
-        console.log("DOM content loaded = " + (p.domContentLoadedEventEnd - p.domContentLoadedEventStart));
-        console.log("DOM complete = " + p.domComplete);
-        console.log("DOM interactive = " + p.interactive);
-
-        // document load and unload time
-        console.log("document load = " + (p.loadEventEnd - p.loadEventStart));
-        console.log("document unload = " + (p.unloadEventEnd - p.unloadEventStart));
-
-        // other properties
-        console.log("type = " + p.type);
-        console.log("redirectCount = " + p.redirectCount);
     }
 }
